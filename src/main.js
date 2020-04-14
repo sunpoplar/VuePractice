f4b8a8f70fcf93cd558ab90cdf231ff4794ba7d6
sunpoplar <momingqimiao0055@163.com>
2020-04-15 00:31:41 +0800

    tabarb样式修改

--- src/main.js
+++ src/main.js
@@ -1,14 +1,18 @@
 import Vue from 'vue'
 import './lib/mui/css/mui.min.css'
+import './lib/mui/css/icons-extra.css'
 
 import {Header} from 'mint-ui'
 Vue.component(Header.name,Header)
 
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+import router from './router.js'
 
 import app from './app.vue'
 
 var vm = new Vue({
     el:'#app',
-    render:c => c(app)
-
+    render:c => c(app),
+    router
 })
--- src/router.js
+++ src/router.js
@@ -1,14 +1,13 @@
 
-import Vue from 'vue'
 import VueRouter from 'vue-router'
-Vue.use(VueRouter)
 
 
 var router = new VueRouter({
     routes:[
        
 
-    ]
+    ],
+    linkActiveClass:"mui-active"//覆盖默认高亮类
 })
 
 export default router 
